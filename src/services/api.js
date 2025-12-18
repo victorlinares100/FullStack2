3bb67221d4ef89465f2f4aee1228cf86446b1426
Pablo <pa.contreras@duocuc.cl>
2025-12-18 19:34:05 -0300

    Cambios para arreglar tokens de login

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 //Así si se modifica el url, solo se cambia en este archivo y no en todos los archivos donde se use axios
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/',
     headers: {
         'Content-Type': 'application/json'
     }
